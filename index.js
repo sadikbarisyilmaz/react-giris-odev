fac3a7ecccaff20080357e9dd3880731e932608a
Sadik Baris Yilmaz <sadikbarisyilmaz@gmail.com>
2022-10-11 20:07:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+async function getData(userId) {
+   
+    const {data: users} = await (axios("https://jsonplaceholder.typicode.com/users/" +`${userId}`))
+
+    const {data: posts} = await (axios("https://jsonplaceholder.typicode.com/posts/"+`${userId}`))
+
+    console.log(users);
+    console.log(posts)
+}
+
+getData(1);
